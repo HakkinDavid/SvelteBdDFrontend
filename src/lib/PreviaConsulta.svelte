27cc2795d9d744ea359351477948ac630c0f7b98
HakkinDavid <desr11@hotmail.com>
2024-10-17 22:21:53 -0700

    me lo destemía

--- src/lib/PreviaConsulta.svelte
+++ src/lib/PreviaConsulta.svelte
@@ -3,10 +3,9 @@
   import { goto } from "$app/navigation";
 
   export let index = 0;
-  export let color = "";
 </script>
 
-<div class="flex flex-col bg-{color}-100 p-2 m-1 border rounded content-between">
+<div class="flex flex-col bg-blue-100 p-2 m-1 border rounded content-between">
   <p class="flex flex-grow">{index + 1 + '. ' + consultas[index]}</p>
-  <button class="bg-{color}-500 text-white px-2 py-1 rounded mt-2" on:click={()=>{goto('/consultas/' + (index + 1))}}>Ver</button>
+  <button class="bg-blue-500 text-white px-2 py-1 rounded mt-2" on:click={()=>{goto('/consultas/' + (index + 1))}}>Ver</button>
 </div>
--- src/routes/consultas/+page.svelte
+++ src/routes/consultas/+page.svelte
@@ -1,12 +1,6 @@
 <script>
   import { número_consultas as n} from "$lib/Consulta.svelte";
   import PreviaConsulta from "$lib/PreviaConsulta.svelte";
-
-  const colors = [
-    'blue',
-    'green',
-    'yellow'
-  ];
 </script>
 
 <svelte:head>
@@ -15,6 +9,6 @@
 
 <section class="grid grid-rows-10 grid-flow-col gap-4 p-1">
   {#each {length: n} as _, i}
-    <PreviaConsulta index={i} color={colors[parseInt(i/10)]}/>
+    <PreviaConsulta index={i}/>
   {/each}
 </section>
