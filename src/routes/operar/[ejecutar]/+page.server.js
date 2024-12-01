465c553f688e1c4a1dcd018fd00a7c33c5885452
HakkinDavid <desr11@hotmail.com>
2024-11-30 19:00:49 -0800

    validation

--- src/routes/operar/[ejecutar]/+page.server.js
+++ src/routes/operar/[ejecutar]/+page.server.js
@@ -1,7 +1,14 @@
 import { mysql_connection } from '$lib/mysql';
 
 export async function load({ params }) {
-  return {
-    resultado: {... (await mysql_connection.query(decodeURIComponent(params.ejecutar)))[0]}
+  try {
+    return {
+      resultado: {... (await mysql_connection.query(decodeURIComponent(params.ejecutar)))[0]}
+    }
+  } catch (e) {
+    return {
+      faulty_query: decodeURIComponent(params.ejecutar),
+      error: JSON.parse(JSON.stringify(e))
+    }
   }
 }
--- src/routes/operar/[ejecutar]/+page.svelte
+++ src/routes/operar/[ejecutar]/+page.svelte
@@ -4,10 +4,13 @@
   export let data;
   
   onMount(() => {
+    if (data.error) {
+      alert(data.error.sqlMessage);
+    }
     history.back();
   });
 </script>
 
 <h1>
-  Cambios efectuados.
+  {#if data && !data.error} Cambios efectuados. {:else if data.error} Revisa tu solicitud. <br> Consulta ejecutada: {data.faulty_query} {:else} Ejecutando... {/if}
 </h1>
